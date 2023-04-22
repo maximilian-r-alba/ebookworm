cad27eaf5f41e4c5a14d82eba09427f97b2185ed
Maximilian Alba <albam476@gmail.com>
2023-04-22 14:27:34 -0400

    add foreign key user_id to chatrooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_20_001728) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_22_180938) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_20_001728) do
     t.string "topic"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_chatrooms_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -49,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_20_001728) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "chatrooms", "users"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "subscriptions"
   add_foreign_key "subscriptions", "chatrooms"
