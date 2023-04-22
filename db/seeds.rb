f30a527bc361734faa7b77bd6418f6d47517268d
Maximilian Alba <albam476@gmail.com>
2023-04-22 12:22:15 -0400

    add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 User.create!(username:"test" , name:"Max Alba", password:"abcd1234", password_confirmation:"abcd1234", profile_picture: "")
+User.create!(username:"test2" , name:"J", password:"abcd1234", password_confirmation:"abcd1234", profile_picture: "")
 
 Chatroom.create!(topic:"We've Got Worm")
+Chatroom.create!(topic: "We've Got Ward")
+Chatroom.create!(topic:"Where the Wild Things Are")
 
 Subscription.create!(user: User.find_by(username:"test"), chatroom:Chatroom.first)
 
