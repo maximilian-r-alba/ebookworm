d18c2cfdb106a844d983a41b6678fd82ba49dbae
Maximilian Alba <albam476@gmail.com>
2023-04-22 14:27:00 -0400

    chatrooms now have an owner on create

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,6 +1,13 @@
 class Chatroom < ApplicationRecord
     validates :topic, presence: true
+    after_create :subscribe_owner
+    belongs_to :owner, class_name: "User", foreign_key: :user_id
     has_many :subscriptions
     has_many :users, through: :subscriptions
     has_many :messages, through: :subscriptions
+
+    private 
+    def subscribe_owner
+        self.owner.subscriptions.create!(chatroom: self)
+    end
 end
