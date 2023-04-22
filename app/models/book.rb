727ecd9b5fe428056290091331d6e5c5a34efdff
Maximilian Alba <albam476@gmail.com>
2023-04-22 17:37:08 -0400

    add unique title validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
     validates_presence_of :title, :author
-
+    validates :title, uniqueness: true
     has_many :reviews
     has_many :users, through: :reviews
 end
