d6d3e1f81789bf5739437cdefcdc230e1e6b791c
Maximilian Alba <albam476@gmail.com>
2023-04-22 12:23:20 -0400

    validate users only have uniq subscription to chat

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,7 @@
 class Subscription < ApplicationRecord
+  # user cannot subscribe to the same chat multiple times
+  validates :chatroom, uniqueness: {scope: :user, message: ": already subscribed to this room"}
   belongs_to :user
   belongs_to :chatroom
-  has_many :messages
+  has_many :messages , dependent: :destroy
 end
