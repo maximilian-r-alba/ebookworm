8cefcf11a334a13c36d1e03200fb909465468b95
Maximilian Alba <albam476@gmail.com>
2023-04-22 14:27:12 -0400

    users have owned_chats

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     validates :username, uniqueness: true
     validates :profile_picture, allow_blank: true, format: {with: %r{.(jpg|png)\Z}i}
     has_secure_password
-
+    has_many :owned_chats, class_name: "Chatroom"
     has_many :subscriptions, dependent: :destroy
     has_many :chatrooms, through: :subscriptions
 end
