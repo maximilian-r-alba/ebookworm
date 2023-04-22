77d94797e54f3486083a886bf408bbcca67da795
Maximilian Alba <albam476@gmail.com>
2023-04-22 15:45:35 -0400

    ensure message chat id is the same as sub chat id

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,13 @@
 class Message < ApplicationRecord
   validates_presence_of :content
+  validate :check_chat
+
   belongs_to :subscription
   belongs_to :chatroom
+  
+  private
+
+  def check_chat
+    errors.add(:chatroom_id, "Chat IDs of subscription and Message must match") unless self.chatroom_id == self.subscription.chatroom_id 
+  end
 end
