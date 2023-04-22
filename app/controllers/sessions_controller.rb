4dfbdd10a5add88357686866b0d39a892e60d36e
Maximilian Alba <albam476@gmail.com>
2023-04-22 19:59:57 -0400

    fix comma typo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
     def create
         
         user = User.find_by(username: params[:username])
-
+        
         if user&.authenticate(params[:password])
             session[:user_id] = user.id
              # might not want/need to see all subscription messages
-            render json: user, include: ['subscriptions', 'subscriptions.messages', 'chatrooms', 'owned_chats'] status: :created
+            render json: user, include: ['subscriptions', 'subscriptions.messages', 'chatrooms', 'owned_chats'], status: :created
         else
            render json: {errors:[ "Invalid Username or password"]}, status: :unauthorized
         end
