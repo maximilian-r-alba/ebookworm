62617227b566dd84eeca5128a2820b413b424970
Maximilian Alba <albam476@gmail.com>
2023-04-22 10:28:24 -0400

    create basic seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create!(username:"test" , name:"Max Alba", password:"abcd1234", password_confirmation:"abcd1234", profile_picture: "")
+
+Chatroom.create!(topic:"We've Got Worm")
+
+Subscription.create!(user: User.find_by(username:"test"), chatroom:Chatroom.first)
+
+User.first.subscriptions.first.messages.create!(chatroom:Chatroom.first, content: "Hellooo Chat")
