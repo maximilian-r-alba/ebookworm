2246388f67ebec157eb02e87f82cde61b9847c74
Maximilian Alba <albam476@gmail.com>
2023-04-22 14:28:01 -0400

    edit seed to reflect chat ownership

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 User.create!(username:"test" , name:"Max Alba", password:"abcd1234", password_confirmation:"abcd1234", profile_picture: "")
 User.create!(username:"test2" , name:"J", password:"abcd1234", password_confirmation:"abcd1234", profile_picture: "")
 
-Chatroom.create!(topic:"We've Got Worm")
-Chatroom.create!(topic: "We've Got Ward")
-Chatroom.create!(topic:"Where the Wild Things Are")
+Chatroom.create!(topic:"We've Got Worm", owner: User.first)
+Chatroom.create!(topic: "We've Got Ward", owner: User.last)
+Chatroom.create!(topic:"Where the Wild Things Are", owner: User.first)
 
-Subscription.create!(user: User.find_by(username:"test"), chatroom:Chatroom.first)
+User.first.subscriptions.create!(chatroom: Chatroom.find_by(topic: "We've Got Ward"))
 
 User.first.subscriptions.first.messages.create!(chatroom:Chatroom.first, content: "Hellooo Chat")
