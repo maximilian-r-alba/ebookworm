f847b9ec97ddbcd9d19b05a1dfe1c552d496c47e
Maximilian Alba <albam476@gmail.com>
2023-04-22 12:25:41 -0400

    add messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,11 @@
 class MessagesController < ApplicationController
+
+    # def index
+    #     render json: Message.all
+    # end
+
+    # def show
+
+    #     render json: Message.find(params[:id])
+    # end
 end
