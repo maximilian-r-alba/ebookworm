d8e90f542b04288347efdb762767193124e0caed
Maximilian Rex Alba <albam476@gmail.com>
2023-04-22 10:14:06 -0400

    create session controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :messages
   resources :chatrooms
   resources :users
+
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
