5d5ed7078c410154b52deaec4b0838ad4a4505c2
Maximilian Alba <albam476@gmail.com>
2023-04-22 15:10:02 -0400

    serialize owned_chats for user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :username, :profile_picture, :headline
   has_many :subscriptions, serializer: UserSubscriptionSerializer
   has_many :chatrooms, through: :subscriptions
+  has_many :owned_chats
 end
