2f0558864e460793dd21ea0108718251e9c48a0f
Maximilian Alba <albam476@gmail.com>
2023-04-22 13:52:57 -0400

    add message params

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,4 +8,9 @@ class MessagesController < ApplicationController
 
     #     render json: Message.find(params[:id])
     # end
+    private
+
+    def message_params
+        params.permit(:chatroom_id , :subscription_id , :content)
+    end
 end
