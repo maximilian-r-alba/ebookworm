4b371e28278f8a33407be2c2804692182800ee61
Maximilian Alba <albam476@gmail.com>
2023-04-22 12:23:40 -0400

    add exception catches for invalid/not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::API
 include ActionController::Cookies
+
 before_action :authorize
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_errors
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_error
 
 private
 
@@ -9,6 +12,14 @@ def authorize
     
     render json: {errors: ["Not authorized"]}, status: :unauthorized unless @current_user
     
+  end
+
+  def render_invalid_errors (invalidObj)
+    render json: {errors: invalidObj.record.errors.full_messages}, status: :unprocessable_entity
+  end
+  
+  def render_not_found_error(invalidObj)
+    render json: {errors: [invalidObj.message]}, status: :not_found
   end
 
 end
