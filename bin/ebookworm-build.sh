9ca35b121dc48368bf1bc1654a32333c4da018f8
Maximilian Alba <albam476@gmail.com>
2023-04-22 22:56:49 -0400

    deployment changes

--- bin/ebookworm-build.sh
+++ bin/ebookworm-build.sh
@@ -1,7 +1,14 @@
 set -o errexit
 
+
 bundle install
 # bundle exec rake assets:precompile # These lines are commented out because we have an API only app
 # bundle exec rake assets:clean
 bundle exec rake db:migrate
-bundle exec rake db:seed
+
+
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
+# bundle exec rake db:seed
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   resources :books, only: [:index, :show, :create]
   resources :messages, only: [:create , :update , :destroy]
   resources :chatrooms
@@ -8,7 +9,11 @@ Rails.application.routes.draw do
 
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
   # Defines the root path route ("/")
   # root "articles#index"
