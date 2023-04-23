3f500de878229f9acda00e2110bbfaeaa3070a7f
Maximilian Alba <albam476@gmail.com>
2023-04-22 20:00:46 -0400

    create navbar

---
+++ client/src/NavBar.js
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import styled from 'styled-components'
+import { UserContext } from "./UserContext";
+function NavBar({setUser}){
+    const user = useContext(UserContext)
+    const navigate = useNavigate()
+
+    function handleLogout(){
+        fetch("/logout", {
+          method: "DELETE"}).then(() => {
+              navigate('/') 
+              setUser(undefined)
+            })
+      }
+    return <div>
+        <StyledNav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/librarysearch">Search</NavLink>
+            {user ? <button onClick={handleLogout}>Logout</button> : <NavLink to="/login"> Login </NavLink>}
+        </StyledNav>
+        
+    </div>
+}
+
+export default NavBar
+
+const StyledNav = styled.nav`
+.active{
+    color: red;
+}
+`
