e79f66509b7a1ae19fac26ffdffa2187b4bfdfef
Maximilian Alba <albam476@gmail.com>
2023-04-28 22:42:25 -0400

    testing cable redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,10 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1") }
+  # config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1") }
+  config.cache_store = :redis_cache_store, {
+    url: ENV['REDIS_URL']
+  }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "https://ebookwormclub-project.onrender.com"
-
+    # origins "*"
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
