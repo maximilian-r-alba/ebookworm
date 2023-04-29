0e7432ea18f7d024ccd2b6ebd7387152a6c60e0d
Maximilian Alba <albam476@gmail.com>
2023-04-28 21:31:24 -0400

    fix browseing books and users

--- client/src/BrowseBooks.js
+++ client/src/BrowseBooks.js
@@ -1,17 +1,30 @@
 import { useNavigate } from "react-router-dom"
 import BookCard from "./BookCard"
 import styled from "styled-components"
+import { useState } from "react"
 function BrowseBooks ({books}) {
 
-    const navigate = useNavigate()
-    function test (book){
-        console.log(book)
-        navigate(`/books/${book.id}`)
-    }
-    return <Container>
-        {books.map((book)=> <BookCard handleClick={test} book={book} /> )}
+    const [filter, setFilter] = useState('title')
+    const [filterParams, setFilterParams] = useState('')
+
+    return <div>
+        <div>
+            <form>
+                Sort by:
+                <select onChange={(e) => setFilter(e.target.value)}>
+                    <option value={'title'}>Title</option>
+                    <option value={'author'}>Author</option>
+                </select>
+                <input type="text" value={filterParams} onChange={(e)=> setFilterParams(e.target.value)} />
+            </form>
+        </div>
+    <Container>
+        {books.filter((book) => book[filter].toUpperCase().includes(filterParams.toUpperCase())).map((book)=> <BookCard key={`${book.id}${book.title}`} book={book} /> )}
 
     </Container>
+
+    </div> 
+    
 }
 
 export default BrowseBooks
@@ -23,6 +36,7 @@ width: 100vw;
 display: flex;
 flex-wrap: wrap;
 justify-content: center;
+align-content: flex-start;
 overflow: scroll;
 overflow-x: hidden;
 `
--- client/src/BrowseUsers.js
+++ client/src/BrowseUsers.js
@@ -2,12 +2,9 @@ import UserCard from "./UserCard"
 import styled from "styled-components"
 
 function BrowseUsers({users}){
-
-    const cards = users.map((user) => <UserCard user={user}/>)
     
     return <CardContainer>
-        {cards}
-
+        {users.map((user) => <UserCard  key={user.id} user={user}/>)}
     </CardContainer>
 }
 
