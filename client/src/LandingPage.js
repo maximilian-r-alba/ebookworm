d48b0e2a06f234404ca63b15aeeb87e0c389dce7
Maximilian Alba <albam476@gmail.com>
2023-04-28 21:31:35 -0400

    reconfigure landing page

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -2,10 +2,11 @@ import { UserContext } from "./UserContext"
 import { useContext } from "react"
 import UserForm from "./UserForm"
 
-function LandingPage(){
+function LandingPage({handleUsers}){
     const user = useContext(UserContext)
+    
     return <>
-    {user ? <h1>Hello {user.name}</h1> : <h1>Hello world</h1>}
+    {user ? <h1>Hello {user.name}</h1> : <UserForm handleUsers={handleUsers}/>}
     </>
 }
 
