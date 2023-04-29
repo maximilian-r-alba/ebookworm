4b95ccdc46bc3995fc49875f94e584073716127c
Maximilian Alba <albam476@gmail.com>
2023-04-28 21:34:22 -0400

    white space

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,11 @@
 class Message < ApplicationRecord
   validates_presence_of :content
   validate :check_chat
-
   belongs_to :subscription
   belongs_to :chatroom
   
   private
-
+  
   def check_chat
     errors.add(:chatroom_id, "Chat IDs of subscription and Message must match") unless self.chatroom_id == self.subscription.chatroom_id 
   end
