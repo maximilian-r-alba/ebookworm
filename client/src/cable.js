b6d1a0ab636950c272bbd77a33f564c704dca40c
Maximilian Alba <albam476@gmail.com>
2023-04-28 23:18:02 -0400

    make ws to wss

--- client/src/cable.js
+++ client/src/cable.js
@@ -1,6 +1,6 @@
 import { createConsumer } from '@rails/actioncable';
 // const URL = 'ws://localhost:3000/cable';
-const URL = "ws://ebookwormclub-project.onrender.com"
+const URL = "wss://ebookwormclub-project.onrender.com"
 const consumer = createConsumer(URL);
  
 export default consumer;
