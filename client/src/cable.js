a7737811f29c665ab39a6774400b930b12734331
Maximilian Alba <albam476@gmail.com>
2023-04-28 23:31:51 -0400

    add port?

--- client/src/cable.js
+++ client/src/cable.js
@@ -1,6 +1,6 @@
 import { createConsumer } from '@rails/actioncable';
 // const URL = 'ws://localhost:3000/cable';
-const URL = "wss://ebookwormclub-project.onrender.com"
+const URL = "wss://ebookwormclub-project.onrender.com:3000"
 const consumer = createConsumer(URL);
  
 export default consumer;
