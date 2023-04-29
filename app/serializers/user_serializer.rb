02dc93e2ac85c2c0b37f1285956fd80686c86481
Maximilian Alba <albam476@gmail.com>
2023-04-28 21:33:36 -0400

    update user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :username, :profile_picture, :headline
   has_many :subscriptions, serializer: UserSubscriptionSerializer
   has_many :chatrooms, through: :subscriptions
+  has_many :reviews
+  has_many :books, through: :reviews
   has_many :owned_chats
 end
