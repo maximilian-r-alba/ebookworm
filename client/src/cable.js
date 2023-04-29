ab2e3736e4768b0025b63ab594758c26c9287f97
Maximilian Alba <albam476@gmail.com>
2023-04-28 23:11:06 -0400

    edit url websocket again

--- client/src/cable.js
+++ client/src/cable.js
@@ -1,6 +1,6 @@
 import { createConsumer } from '@rails/actioncable';
 // const URL = 'ws://localhost:3000/cable';
-const URL = "https://ebookwormclub-project.onrender.com"
+const URL = "ws://ebookwormclub-project.onrender.com"
 const consumer = createConsumer(URL);
  
 export default consumer;
