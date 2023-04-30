f45925976f024d9ed171839a86849657a0b35837
Maximilian Alba <albam476@gmail.com>
2023-04-30 16:32:02 -0400

    seed

--- bin/ebookworm-build.sh
+++ bin/ebookworm-build.sh
@@ -2,15 +2,14 @@ set -o errexit
 
 
 bundle install
-RAILS_ENV=production rake db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1 
+
 # bundle exec rake assets:precompile # These lines are commented out because we have an API only app
 # bundle exec rake assets:clean
 bundle exec rake db:migrate
-
+bundle exec rake db:seed
 
 
 rm -rf public
 npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
-# bundle exec rake db:seed
