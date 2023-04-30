d8bdb1c0eb80d02353f447a216a3eecee718f7b3
Maximilian Alba <albam476@gmail.com>
2023-04-30 12:18:13 -0400

    add chat form to page

--- client/src/BrowseChats.js
+++ client/src/BrowseChats.js
@@ -1,8 +1,41 @@
 import ChatCard from "./ChatCards";
+import { useState } from "react";
+import styled from "styled-components";
+import ChatroomForm from "./ChatroomForm";
 
-export default function BrowseChats({chatrooms}){
+export default function BrowseChats({ user , formatChat , chatrooms}){
 
-    return<div>
+   
+    const [formView, setFormView] = useState(false)
+
+
+
+    return<RouteDiv>
+        {user ? <h2 onClick={() => {setFormView(!formView)}}>{formView ? "Submit a Topic" : "Create New Chat?"}</h2> : <></>}
+        {formView ? <ChatroomForm setFormView={setFormView} formatChat={formatChat} /> : <></>}
+        <div className="chatContainer">
         {chatrooms.map((chat) => <ChatCard key={chat.id} chat={chat} />)}
-    </div>
+        </div>
+      
+    </RouteDiv>
 }
+
+const RouteDiv = styled.div`
+
+width: 100%;
+h2{
+    cursor: pointer;
+}
+div.chatContainer{
+    width: 100%;
+    height: 80vh;
+    display: flex;
+    flex-direction: column;
+    div{
+        width: 80%;
+    }
+    overflow: hidden;
+    overflow-y: scroll;
+ 
+}
+`
