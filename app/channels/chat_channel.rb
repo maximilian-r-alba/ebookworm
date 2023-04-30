936593a55b570fcdbb6c3b1990d460785f9c8b21
Maximilian Alba <albam476@gmail.com>
2023-04-30 13:56:52 -0400

    delete unneeded comments

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,9 +1,7 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
     chat = Chatroom.find(params[:room])
-    # stream_from "chatroom#{params[:room]}"
-    # stream_from "chat"
+
     stream_for chat
   end
 
