8c46b4980ea597ad05d2161555bcb61aea8648f1
Maximilian Alba <albam476@gmail.com>
2023-04-30 12:18:31 -0400

    conditionally render landing page

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -1,13 +1,39 @@
 import { UserContext } from "./UserContext"
 import { useContext } from "react"
 import UserForm from "./UserForm"
+import styled from "styled-components"
+import { useNavigate } from "react-router-dom"
 
 function LandingPage({handleUsers}){
     const user = useContext(UserContext)
-    
-    return <>
-    {user ? <h1>Hello {user.name}</h1> : <UserForm handleUsers={handleUsers}/>}
+    const navigate = useNavigate()
+    return <StyledLanding>
+    {user ? 
+    <>
+   {navigate('/library')}
     </>
+    
+    
+    : 
+    <div className="formContainer">
+        <UserForm handleUsers={handleUsers}/>
+    </div>
+    }
+    </StyledLanding>
 }
 
 export default LandingPage
+
+const StyledLanding = styled.div`
+
+width: 100vw;
+height: 80vh;
+div.formContainer{
+    margin-left: auto;
+    margin-right: auto;
+    width: fit-content;
+    border: 2px solid;
+    border-radius: 20px;
+    background-color: white;
+}
+`
