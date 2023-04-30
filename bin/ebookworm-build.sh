a17b35c0e2ff7d988250f6f815defbf4205e7d14
Maximilian Alba <albam476@gmail.com>
2023-04-30 16:29:07 -0400

    drop db

--- bin/ebookworm-build.sh
+++ bin/ebookworm-build.sh
@@ -2,11 +2,11 @@ set -o errexit
 
 
 bundle install
-
+RAILS_ENV=production rake db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1 
 # bundle exec rake assets:precompile # These lines are commented out because we have an API only app
 # bundle exec rake assets:clean
 bundle exec rake db:migrate
-RAILS_ENV=production rake db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1 
+
 
 
 rm -rf public
