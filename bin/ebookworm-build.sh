d62afe1e19cab2b34effc407a6ee668a7dca727f
Maximilian Alba <albam476@gmail.com>
2023-04-30 16:11:02 -0400

    replant

--- bin/ebookworm-build.sh
+++ bin/ebookworm-build.sh
@@ -5,7 +5,7 @@ bundle install
 # bundle exec rake assets:precompile # These lines are commented out because we have an API only app
 # bundle exec rake assets:clean
 bundle exec rake db:migrate
-bundle exec rake db:seed
+bundle exec rake db:seed:replant
 
 rm -rf public
 npm install --prefix client && npm run build --prefix client
