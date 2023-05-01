e3075db447457f4ff3b5976f44e1201b585dcc73
Maximilian Alba <albam476@gmail.com>
2023-04-30 22:27:54 -0400

    change to string stream from

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -2,7 +2,8 @@ class ChatChannel < ApplicationCable::Channel
   def subscribed
     
     # chat = Chatroom.find(params[:room])
-    stream_for "chatroom#{params[:room]}"
+    # stream_for chat
+    stream_from "chatroom#{params[:room]}"
   end
 
   def unsubscribed
